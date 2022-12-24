92470a3bada1d0cce0fced90f72c6ea5dff34fb8
Nishant Racherla <nishant.racherla@gmail.com>
2022-12-23 17:05:00 -0800

    renamed app to match project name

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,11 @@ module Resonance
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: '_resonance_session',
+      same_site: :lax,
+      secure: Rails.env.production?
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
