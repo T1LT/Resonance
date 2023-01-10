65e130876b03aec4b730875ecc5988dc026e6768
Nishant Racherla <nishant.racherla@gmail.com>
2023-01-10 12:27:48 -0800

    added more homepage styling

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -3,6 +3,11 @@ import "./Homepage.css";
 import logo from "../../assets/logo.png";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import section2 from "../../assets/section-2.svg";
+import section3 from "../../assets/section-3.svg";
+import section4 from "../../assets/section-4.svg";
+import section5 from "../../assets/section-5.svg";
+import sparkles from "../../assets/sparkles.svg";
 
 const Homepage = () => {
   const sessionUser = useSelector((store) => store.session.user);
@@ -61,40 +66,71 @@ const Homepage = () => {
         </div>
       </div>
 
-      <div className="section2">
-        <h1>Create an invite- only place where you belong</h1>
-        <p>
-          Resonance servers are organized into topic-based channels where you
-          can collaborate, share, and just talk about your day without clogging
-          up a group chat.
-        </p>
+      <div className="section-hero">
+        <div className="section-container">
+          <div className="image-container">
+            <img src={section2} alt="invite users image" />
+          </div>
+          <div className="section-content">
+            <h1 className="section-header">
+              Create an invite- only place where you belong
+            </h1>
+            <p className="section-text">
+              Resonance servers are organized into topic-based channels where
+              you can collaborate, share, and just talk about your day without
+              clogging up a group chat.
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className="section3">
-        <h1>Where hanging out is easy</h1>
-        <p>
-          Grab a seat in a text channel when you're free. Friends in your server
-          can see you're around and instantly pop in to chat.
-        </p>
+      <div className="section-hero section-alt">
+        <div className="section-container">
+          <div className="section-content">
+            <h1 className="section-header">Where hanging out is easy</h1>
+            <p className="section-text">
+              Grab a seat in a text channel when you're free. Friends in your
+              server can see you're around and instantly pop in to chat.
+            </p>
+          </div>
+          <div className="image-container">
+            <img src={section3} alt="text channels" />
+          </div>
+        </div>
       </div>
 
-      <div className="section4">
-        <h1>From few to a fandom</h1>
-        <p>
-          Get any community running with powerful moderation tools. Setup text
-          servers, channels and more with ease.
-        </p>
+      <div className="section-hero">
+        <div className="section-container">
+          <div className="image-container">
+            <img src={section4} alt="moderation" />
+          </div>
+          <div className="section-content">
+            <h1 className="section-header">From few to a fandom</h1>
+            <p className="section-text">
+              Get any community running with powerful moderation tools. Setup
+              text servers, channels and more with ease.
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className="section5">
-        <h1>RELIABLE TECH FOR STAYING CLOSE</h1>
-        <p>
-          Low-latency texting feels like you're in the same room. Gather up and
-          chat with your friends in real-time.
-        </p>
+      <div className="section5 section-alt">
+        <div className="section-container-bottom">
+          <div className="section-content-bottom">
+            <h1 className="section-header-bottom">
+              RELIABLE TECH FOR STAYING CLOSE
+            </h1>
+            <p className="section-text">
+              Low-latency texting feels like you're in the same room. Gather up
+              and chat with your friends in real-time.
+            </p>
+            <img src={section5} alt="reliable tech" className="section5-img" />
+          </div>
+        </div>
       </div>
 
-      <div className="section6">
+      <div className="section6 section-alt">
+        <img src={sparkles} alt="sparkles" className="sparkles" />
         <h1>Ready to start your journey?</h1>
         <button
           className="homepage-button blue-button"
