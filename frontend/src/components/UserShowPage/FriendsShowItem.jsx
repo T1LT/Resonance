03b69aadcc65ffa798902f64ce58455b4c1a6e1f
Nishant Racherla <nishant.racherla@gmail.com>
2023-01-09 17:07:02 -0800

    fixed hover colors for friendship resolution buttons

--- frontend/src/components/UserShowPage/FriendsShowItem.jsx
+++ frontend/src/components/UserShowPage/FriendsShowItem.jsx
@@ -8,15 +8,21 @@ import YesIcon from "@mui/icons-material/Check";
 import NoIcon from "@mui/icons-material/Close";
 import { useDispatch } from "react-redux";
 import { deleteFriendship, updateFriendship } from "../../store/friendship";
+import { useState } from "react";
 
 const FriendsShowItem = ({ friendTab, friendObj, friendships }) => {
   const friend = friendObj.friend;
   const dispatch = useDispatch();
   const friendshipReceiver = !!friendObj.friend.user2Id;
+  const [hovered, setHovered] = useState(false);
+  const [acceptHovered, setAcceptHovered] = useState(false);
+  const [ignoreHovered, setIgnoreHovered] = useState(false);
 
   const handleAcceptInvite = (e) => {
     e.preventDefault();
-    const friendship = friendships.find(el => el.id === friendObj.friendshipId);
+    const friendship = friendships.find(
+      (el) => el.id === friendObj.friendshipId
+    );
     console.log(friendships);
     const friendshipData = { ...friendship, status: "friends" };
     dispatch(updateFriendship(friendshipData));
@@ -30,13 +36,25 @@ const FriendsShowItem = ({ friendTab, friendObj, friendships }) => {
   return (
     <>
       <div className="options-divider" id="user-divider"></div>
-      <li className="friend-show-li">
+      <li
+        className="friend-show-li"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
         <div className="friend-item-left">
           <div className="user-squircle" id={randomColor(friend.id)}>
             <img src={logo} alt="logo" className="user-logo" />
           </div>
           <div className="li-user-details">
-            <p className="user-text">{friend.username}</p>
+            <p className="user-text">
+              {friend.username}
+              <span
+                className="hidden-user-tag"
+                id={hovered ? "show-element" : undefined}
+              >
+                #{friend.tag}
+              </span>
+            </p>
             {friendTab === "pending" ? (
               <>
                 {friendshipReceiver ? (
@@ -64,10 +82,14 @@ const FriendsShowItem = ({ friendTab, friendObj, friendships }) => {
                     <div
                       className="user-squircle user-item-option"
                       onClick={handleAcceptInvite}
+                      onMouseEnter={() => setAcceptHovered(true)}
+                      onMouseLeave={() => setAcceptHovered(false)}
                     >
                       <YesIcon
-                        sx={{ fontSize: "18px", color: "#DCDDDE" }}
-                        className="accept-icon"
+                        sx={{ fontSize: "18px" }}
+                        className={
+                          acceptHovered ? "accept-icon-hover" : "accept-icon"
+                        }
                       />
                     </div>
                   </BootstrapTooltip>
@@ -80,10 +102,14 @@ const FriendsShowItem = ({ friendTab, friendObj, friendships }) => {
                     <div
                       className="user-squircle user-item-option"
                       onClick={handleIgnoreInvite}
+                      onMouseEnter={() => setIgnoreHovered(true)}
+                      onMouseLeave={() => setIgnoreHovered(false)}
                     >
                       <NoIcon
-                        sx={{ fontSize: "18px", color: "#DCDDDE" }}
-                        className="ignore-icon"
+                        sx={{ fontSize: "18px" }}
+                        className={
+                          ignoreHovered ? "ignore-icon-hover" : "ignore-icon"
+                        }
                       />
                     </div>
                   </BootstrapTooltip>
