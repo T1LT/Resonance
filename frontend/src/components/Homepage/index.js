25d0ce40a9428faffbad2f193c74707b3e00400d
Nishant Racherla <nishant.racherla@gmail.com>
2023-01-31 14:29:48 -0800

    fixed portfolio link

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -51,7 +51,8 @@ const Homepage = () => {
               Linkedin
             </a>
             <a
-              href="https://www.nishantracherla.com/"
+              href="https://nishantracherla.com/"
+              target="_blank"
               className="homepage-link"
             >
               Portfolio
